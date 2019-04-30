97b4468a22b52db42f2c6331fe8aafd295c4ce32
ctritta <ctritta@timsoft.com>
2019-04-30 17:06:38 +0200

    io.js suppr hex2a

--- io/io.js
+++ io/io.js
@@ -1,12 +1,6 @@
 const fs = require('fs');
 
 
-function hex2a(hex) {
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
-
 module.exports.decodeHexFileContent = (filePath) =>  {
     return new Promise((resolve, reject) => {
 
